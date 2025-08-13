0ff6150d6890862fe58ff95c3ffd12265a3d74fe
rocillorn2021-create <rocillorn2021@licifug.ugto.mx>
2025-08-13 16:14:03 -0600

    Add files via upload

---
+++ Semana2/ago13_e1.c
@@ -0,0 +1,17 @@
+//Naomi Eliane Rocillo Rangel
+//Realizar un ejercicio que imprima los resultados de las operaciones
+
+ #include <stdio.h>
+ #include <math.h>
+
+int main ()
+{
+  int a=3, b=4, c=5, d=7;
+  int e;
+
+  printf("e=(a+b)*c/d =%d\n",(a+b)*c/d);
+  printf("e=((a+b)*c)/d =%d\n",((a+b)*c)/d);
+  printf("((a+b)*c)/d =%d\n",a+b*c/d);
+  printf("e=a+(b*c)/d =%d\n",e=a+(b*c)/d);
+
+}
